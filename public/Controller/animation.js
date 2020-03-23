f0a668dfc9fba3b67eee7f18f0ef5d4af8931983
Mark Bonney <mark_b96@protonmail.com>
2020-03-23 12:40:55 +0000

    Added button transitions

--- public/Controller/animation.js
+++ public/Controller/animation.js
@@ -19,18 +19,28 @@ const workout = function () {
     const w1 = document.querySelector('.w1');
     const days = document.querySelector(".days");
     const d1 = document.querySelector('.d1');
+    const dArr = document.querySelectorAll("li");
 
 
     days.addEventListener('click', function (e) {
-        if (e.target && e.target.matches("li.d1")) {
-            w1.classList.toggle("w1-active");
-            days.classList.toggle("days-hide");
-            d1.classList.toggle("day-show");
-        }
+
+        checked = !checked;
+        w1.classList.toggle('w1-active');
+        var clicked = '.' + e.target.classList[0];
+
+        const t = document.querySelector(clicked);
+
+        dArr.forEach(li => {
+            console.log(li.className);
+            if (li.className != t.className && checked)
+                li.remove();
+            if (li.className != t.className && !checked)
+                days.append(li);
+        })
     })
 }
 
-
+var checked = false;
 
 
 workout();
