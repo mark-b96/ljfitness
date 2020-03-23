f885273899c408ce5836ef3a49ecc04cdfaffa2b
Mark Bonney <mark_b96@protonmail.com>
2020-03-23 10:53:24 +0000

    Added content

--- public/Controller/animation.js
+++ public/Controller/animation.js
@@ -1,15 +1,37 @@
 
-const slide = function () {
+const slideMenu = function () {
     const logo = document.querySelector(".logo");
     const menu = document.querySelector(".menu");
     const days = document.querySelector(".days");
+    const content = document.querySelector(".content");
 
     logo.addEventListener('click', () => {
         logo.classList.toggle('logo-colour');
         menu.classList.toggle('logo-active');
         days.classList.toggle('days-active');
+        content.classList.toggle('content-active');
     })
 
 }
 
-slide();
+const workout = function () {
+
+    const w1 = document.querySelector('.w1');
+    const days = document.querySelector(".days");
+    const d1 = document.querySelector('.d1');
+
+
+    days.addEventListener('click', function (e) {
+        if (e.target && e.target.matches("li.d1")) {
+            w1.classList.toggle("w1-active");
+            days.classList.toggle("days-hide");
+            d1.classList.toggle("day-show");
+        }
+    })
+}
+
+
+
+
+workout();
+slideMenu();
